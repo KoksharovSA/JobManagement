38693aed083bb7d4806a4f8af68b9011b3e63fad
SERGEY KOKSHAROV <37808925+KoksharovSA@users.noreply.github.com>
2022-10-30 16:12:41 +0700

    Update settings.py

--- job_management/job_management/settings.py
+++ job_management/job_management/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%^c9ct-g$epq&1gjm)m@&f86*)3jso&*3d-5x+vpd#(1z(m-&)'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
