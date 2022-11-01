164f5ea852206503243c99a329e28c6391af3a41
KoksharovSA_LT <koksharovsa@gmail.com>
2022-11-01 21:41:44 +0700

    Commit before creation new apps

--- job_management/home/views.py
+++ job_management/home/views.py
@@ -5,7 +5,10 @@ from .models import *
 
 def home(request):
     posts = HomePage.objects.all()
-    return render(request, 'home/home.html', {'title': 'Главная страница', 'posts': posts})
+    return render(request, 'home/home.html', {'title': 'Главная страница',
+                                              'css_active': 'nav-link px-2 fs-4 fw-bold text-warning',
+                                              'css_nonactive': 'nav-link px-2 fs-4 text-white',
+                                              'posts': posts})
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
